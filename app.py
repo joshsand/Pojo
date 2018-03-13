c687285b3abc02c5958b363648e304cc87ebf202
Josh Sand <contact@joshsand.com>
2018-03-13 14:43:07 -0700

    Converting to worker process, removing Flask

--- app.py
+++ app.py
@@ -1,45 +1,28 @@
-import threading, random, asyncio
-from os import environ
-from flask import Flask
-import discord
-	
-'''
-POJO THREAD (Discord.py server)
-'''
+import discord, random
 
 client = discord.Client()
-loop = asyncio.new_event_loop()
 
-def run_pojo(loop):
-	asyncio.set_event_loop(loop)
-	
-	@client.event
-	async def on_ready():
-		print(' * Pojo awakes')
-	
-	@client.event
-	async def on_message(message):
-		# Ignore messages from self
-		if message.author == client.user:
-			return
+@client.event
+async def on_message(message):
+	# Ignore messages from self
+	if message.author == client.user:
+		return
 		
-		await someoneSayPojo(message)
-	
-	client.run('NDE5NzYzMjY1NjMyMDc1Nzc3.DX02ww.vj2GObEpqMeZEYswpbNzhwW7Zkw')
+	await someoneSayPojo(message)
 	
 async def someoneSayPojo(message):
 	# Let's see Jerran get around this one
 	pojos = ["pojo", "p_o_j_o", "p o j o", "p-o-j-o", "plain old java object"]
-
+	
 	# Lowercase
 	msg = message.content.lower()
-
+	
 	# Remove double spaces
 	msg = ' '.join(msg.split())
-
+	
 	# Convert zeros to O's
 	msg = msg.replace('0', 'O')
-
+	
 	# If any of the pojo matches are in msg...
 	if any(pojo in msg for pojo in pojos):
 		responses = [
@@ -64,29 +47,12 @@ async def someoneSayPojo(message):
 				'𝖕𝖔𝖏𝖔',
 				'𝕡𝕠𝕛𝕠'
 			]
-		
+			
 		response = random.choice(responses)
 		await client.send_message(message.channel, response)
-
-'''
-SERVER THREAD (Flask server)
-'''
+		
+@client.event
+async def on_ready():
+	print('Pojo awakes')
 	
-app = Flask(__name__)
-	
-def run_server():
-	app.run(environ.get('PORT'))
-	
-@app.route('/')
-def index():
-	return "Pojo"
-	
-'''
-MAIN
-'''
-	
-if __name__=='__main__':
-	server_thread = threading.Thread(target=run_server)
-	pojo_thread = threading.Thread(target=run_pojo, args=(loop,))
-	server_thread.start()
-	pojo_thread.start()
+client.run('NDE5NzYzMjY1NjMyMDc1Nzc3.DX02ww.vj2GObEpqMeZEYswpbNzhwW7Zkw')
