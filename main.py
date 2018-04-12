816567bb0006fb77d4c29c0a7d8fd1ae4624fbbf
Josh Sand <contact@joshsand.com>
2018-04-12 00:36:03 -0700

    Token now pulled from config variable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import os
 from app.handlers.messagehandler import MessageHandler
 
 client = discord.Client()
@@ -9,11 +10,15 @@ async def on_message(message):
 	# Ignore messages from self
 	if message.author == client.user:
 		return
-		
+
 	await handler.parse(message)
-		
+
 @client.event
 async def on_ready():
 	print('Pojo awakes')
-	
-client.run('NDE5NzYzMjY1NjMyMDc1Nzc3.DX02ww.vj2GObEpqMeZEYswpbNzhwW7Zkw')
+
+if 'BOT_TOKEN' in os.environ:
+	client.run(os.environ['BOT_TOKEN'])
+else:
+	print("ERROR: Client did not start. 'BOT_TOKEN' not found in environment variables.")
+	client.close()
